b4468b7b1c10789b022f395858122c681c7bed24
KulR <Kulekov.Roman@yandex.ru>
2018-10-02 11:56:19 +0300

    homework 1

---
+++ hw1/src/lib/test.js
@@ -0,0 +1,8 @@
+const say = function (name) {
+    var div = document.createElement("p");
+    div.className = "block";
+    div.innerHTML = 'Hello, ' + name;
+    document.body.appendChild(div);
+}
+
+export default say;
